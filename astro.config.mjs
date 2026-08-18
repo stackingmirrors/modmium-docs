3c61fe0ac8dbe92daaa2e3608cd3e4a56530dcbd
crystal ball <yaoilover87@proton.me>
2026-08-17 20:09:15 -0400

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,29 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Modmium',
+			social: [
+				{
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/crosmium/modmium',
+				},
+			],
+
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Docs',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Building', slug: 'docs/building' },
+						{ label: 'Contributing', slug: 'docs/contributing' },
+						{ label: 'Dependencies', slug: 'docs/dependencies' },
+						{ label: 'Device Policy Editor', slug: 'docs/device-policy-editor' },
+						{ label: 'Installation', slug: 'docs/installation' },
+						{ label: 'Unbricking', slug: 'docs/unbricking' },
+						{ label: 'Usage', slug: 'docs/usage' },
+						{ label: 'User Policy Editor', slug: 'docs/user-policy-editor' },
+						{ label: 'vboot-utils', slug: 'docs/vboot-utils' },
 					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
 				},
 			],
 		}),
